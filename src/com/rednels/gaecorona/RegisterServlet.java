256d93a6431594c576156c9f1a0ddca0b1ba3215
unknown <GSlender@LAP-304099.services.esriaustralia.com.au>
2013-08-22 07:06:21 +1000

    Added basic mail support

--- src/com/rednels/gaecorona/RegisterServlet.java
+++ src/com/rednels/gaecorona/RegisterServlet.java
@@ -3,8 +3,20 @@ package com.rednels.gaecorona;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Properties;
 
 import javax.servlet.http.*;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Multipart;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
 
 
 @SuppressWarnings("serial")
@@ -31,6 +43,35 @@ public class RegisterServlet extends HttpServlet {
         PrintWriter out = response.getWriter();
         WebServiceResponse wsr = null;
         
+        Properties props = new Properties();
+        Session session = Session.getDefaultInstance(props, null);
+
+        String plainBody = "...";
+    	String htmlBody = "<html>";        // ...
+
+        try {
+
+            Multipart mp = new MimeMultipart();
+            // Unformatted text version
+            final MimeBodyPart textPart = new MimeBodyPart();
+            textPart.setText(plainBody);
+            mp.addBodyPart(textPart);
+            // HTML version
+            MimeBodyPart htmlPart = new MimeBodyPart();
+            htmlPart.setContent(htmlBody, "text/html");
+            mp.addBodyPart(htmlPart);
+            
+            Message msg = new MimeMessage(session);
+            msg.setFrom(new InternetAddress("admin@example.com", "Example.com Admin"));            
+            msg.setSubject("Your Example.com account has been activated");
+            msg.setContent(mp);
+            Transport.send(msg);
+
+        } catch (AddressException e) {
+            // ...
+        } catch (MessagingException e) {
+            // ...
+        }
         // send an email to the user about how to register
 //		if (cred.getUsername().equals("user.name") && cred.getPassword().equals("s3cr3t")) {
 //			wsr = new WebServiceResponse(WebServiceResponse.Status.OK, "Login success!");
